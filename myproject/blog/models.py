d7b5a50dc7d2a6c3ae9add1c7c5c441f121798dd
moteily <994283977@qq.com>
2020-08-17 16:09:43 +0800

    never-give-up

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 
 
 class BlogArticles(models.Model):
-    title = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)   #字符串类型，必须要给出max_length,对应于html中的<input type="text"> 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")  # CASCADE:级联删除，
     # 如果删除了“用户表”中某个用户，文章表中用户的文章记录也会删除
     body = models.TextField()
+    # iddd = models.DateField(auto_now=...)
     publish = models.DateTimeField(default=timezone.now)
-
     class Meta:
         ordering = ("-publish",)  # 元组只有一个元素时，要加，
 
